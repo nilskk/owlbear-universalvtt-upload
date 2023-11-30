4546f60505bcd9c812ceb783916ad0df1a066314
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2023-11-30 21:53:43 +0100

    convert png to jpg first

--- src/importUniversalVTT.ts
+++ src/importUniversalVTT.ts
@@ -3,97 +3,111 @@ import { simplifyPolyline } from "./simplify"
 import path from "path-browserify";
 
 
-var jsonData: any
-
+let jsonData: any
 
 /**
- * Creates a scene from the input element.
- * 
- * @param element - The HTMLInputElement to create the scene from.
+ * Reads a file from an input element, parses it as JSON, and updates the jsonData variable.
+ * @param element - The input element to read the file from.
  */
 export function createSceneFromInput(element: HTMLInputElement) {
+    // Add a change event listener to the input element
     element.addEventListener('change', () => {
+        // If there are no files selected, return early
         if (!element.files) return;
-        const file = element.files[0]
+
+        // Get the first file from the input element
+        const file = element.files[0];
+
+        // If a file is selected
         if (file) {
+            // Create a new FileReader
             const fileReader = new FileReader();
+
+            // Add a load event listener to the FileReader
             fileReader.onload = () => {
-                const fileContent = fileReader.result as string
-                jsonData = JSON.parse(fileContent)
-                jsonData.name = path.basename(file.name, path.extname(file.name))
+                // Parse the file content as JSON and assign it to the jsonData variable
+                jsonData = JSON.parse(fileReader.result as string);
 
-
+                // Update the name property of jsonData with the base name of the file
+                jsonData.name = path.basename(file.name, path.extname(file.name));
             };
-            fileReader.readAsText(file)
+
+            // Start reading the file as text
+            fileReader.readAsText(file);
         }
     });
 }
 
 /**
  * Uploads a scene based on the provided button and checkbox elements.
- * 
- * @param buttonElement - The button element that triggers the scene upload.
- * @param checkboxElement - The checkbox element that determines whether to include lights in the scene.
+ * @param buttonElement - The button element that triggers the upload.
+ * @param checkboxElement - The checkbox element that determines whether lights should be included.
+ * @param simplificationCheckbox - The checkbox element that determines whether polyline simplification should be applied.
+ * @param rangeElement - The range input element that determines the simplification factor.
  */
-export function uploadScene(buttonElement: HTMLButtonElement, checkboxElement: HTMLInputElement, simplificationCheckbox:HTMLInputElement, rangeElement:HTMLInputElement) {
+export function uploadScene(buttonElement: HTMLButtonElement, checkboxElement: HTMLInputElement, simplificationCheckbox: HTMLInputElement, rangeElement: HTMLInputElement) {
+    // Add a click event listener to the button element
     buttonElement.addEventListener('click', async () => {
-        const file = await convertBase64toImageFile(jsonData.image)
-        const image = buildImageUpload(file)
+        console.log("Button clicked");
+        // Convert the base64 image to a file
+        const file = base64ToImageFile(jsonData.image)
+        const jpegBlob = await convertPngFileToJpegBlob(file)
+        const jpegFile = new File([jpegBlob], 'filename.jpeg', { type: 'image/jpeg' });
+
+        // Build the image upload
+        const image = buildImageUpload(jpegFile)
             .dpi(jsonData.resolution.pixels_per_grid)
             .name(jsonData.name)
             .build()
 
-        var sceneItems: Item[] = []
-        jsonData.line_of_sight.forEach((element: any) => {
-            if (simplificationCheckbox.checked == true){
+        // Map the line of sight data to wall items
+        const sceneItems: Item[] = jsonData.line_of_sight.map((element: any) => {
+            // If the simplification checkbox is checked, simplify the polyline
+            if (simplificationCheckbox.checked) {
                 element = simplifyPolyline(element, rangeElement.valueAsNumber)
             }
-            const item = createWallFromPoints(element)
-            sceneItems.push(item)
-        });
-        jsonData.portals.forEach((element: any, index: number) => {
-            const item = createDoorFromPoints(element.bounds, index)
-            sceneItems.push(item)
-        });
-        if (checkboxElement.checked == true) {
-            jsonData.lights.forEach((element: any, index: number) => {
-                const item = createLightsFromPoints(element.position, index, jsonData.resolution.pixels_per_grid, element.range)
-                sceneItems.push(item)
-            });
-        }
-        
 
+            // Create a wall from the points
+            return createWallFromPoints(element)
+        });
+
+        // Map the portal data to door items and add them to the scene items
+        sceneItems.push(...jsonData.portals.map((element: any, index: number) => createDoorFromPoints(element.bounds, index)));
+
+        // If the checkbox is checked, map the light data to light items and add them to the scene items
+        if (checkboxElement.checked) {
+            sceneItems.push(...jsonData.lights.map((element: any, index: number) => createLightsFromPoints(element.position, index, jsonData.resolution.pixels_per_grid, element.range)));
+        }
+
+        // Build the scene upload
         const scene = buildSceneUpload()
             .baseMap(image)
             .name(jsonData.name)
             .items(sceneItems)
             .build()
 
-        // scene.grid.dpi = jsonData.resolution.pixels_per_grid
-
+        // Upload the scene
         OBR.assets.uploadScenes([scene])
-
     })
-
 }
 
 
 /**
  * Creates a wall from an object containing points.
- * 
  * @param pointsObject - The object containing the points for the wall.
  * @returns The created wall item.
  */
-function createWallFromPoints(pointsObject: any) {
-    const itemScale = {
-        x: 150,
-        y: 150
-    }
+function createWallFromPoints(pointsObject: any): Item {
+    // Define common scale for items
+    const itemScale = { x: 150, y: 150 }
 
+    // Define metadata for the item
     const itemMetadata: Metadata = {
         'com.battle-system.smoke/isVisionLine': true
     }
-    const item = buildCurve()
+
+    // Build and return the item
+    return buildCurve()
         .points(pointsObject)
         .metadata(itemMetadata)
         .scale(itemScale)
@@ -105,29 +119,27 @@ function createWallFromPoints(pointsObject: any) {
         .fillOpacity(0)
         .tension(0)
         .build()
-    return item
 }
 
 /**
  * Creates a door from the given points object and door ID.
- * 
  * @param pointsObject - The points object defining the shape of the door.
  * @param doorId - The ID of the door.
  * @returns The created door item.
  */
-function createDoorFromPoints(pointsObject: any, doorId: number) {
-    const itemScale = {
-        x: 150,
-        y: 150
-    }
+function createDoorFromPoints(pointsObject: any, doorId: number): Item {
+    // Define common scale for items
+    const itemScale = { x: 150, y: 150 }
 
+    // Define metadata for the item
     const itemMetadata: Metadata = {
         'com.battle-system.smoke/isVisionLine': true,
         'com.battle-system.smoke/isDoor': true,
         'com.battle-system.smoke/doorId': doorId,
     }
 
-    const item = buildCurve()
+    // Build and return the item
+    return buildCurve()
         .points(pointsObject)
         .metadata(itemMetadata)
         .scale(itemScale)
@@ -139,22 +151,22 @@ function createDoorFromPoints(pointsObject: any, doorId: number) {
         .fillOpacity(0)
         .tension(0)
         .build()
-    return item
 }
 
 /**
- * Creates lights from points.
- * 
- * @param pointsObject - The points object.
- * @param lightId - The light ID.
- * @param dpi - The DPI (dots per inch).
- * @param range - The range of the vision.
- * @returns The created item.
+ * Creates a light from the given points object, light ID, DPI, and range.
+ * @param pointsObject - The points object defining the position of the light.
+ * @param lightId - The ID of the light.
+ * @param dpi - The DPI (dots per inch) for the light's image.
+ * @param range - The range of the light.
+ * @returns The created light item.
  */
-function createLightsFromPoints(pointsObject: any, lightId: number, dpi: number, range: number) {
-    pointsObject.x = pointsObject.x * 150
-    pointsObject.y = pointsObject.y * 150
+function createLightsFromPoints(pointsObject: any, lightId: number, dpi: number, range: number): Item {
+    // Scale the points object
+    pointsObject.x *= 150
+    pointsObject.y *= 150
 
+    // Define metadata for the light
     const itemMetadata: Metadata = {
         'com.battle-system.smoke/visionTorch': true,
         'com.battle-system.smoke/hasVision': true,
@@ -162,35 +174,80 @@ function createLightsFromPoints(pointsObject: any, lightId: number, dpi: number,
         'com.battle-system.smoke/visionRange': range,
     }
 
-    const item = buildImage(
+    // Build and return the light item
+    return buildImage(
         {
             height: dpi,
             width: dpi,
             url: "https://nilskk.github.io/owlbear-universalvtt-upload/transparent.png",
             mime: "image/png",
         },
-        { dpi: dpi, offset: { x: dpi/2, y: dpi/2 } }
+        { dpi: dpi, offset: { x: dpi / 2, y: dpi / 2 } }
     )
         .position(pointsObject)
         .metadata(itemMetadata)
         .layer("CHARACTER")
-        .name("Light " + lightId)
-        .plainText("Light " + lightId)
+        .name(`Light ${lightId}`)
+        .plainText(`Light ${lightId}`)
         .locked(true)
         .build()
-    return item
+}
+
+
+
+/**
+ * Converts a base64 string to an image File object.
+ * 
+ * @param base64 - The base64 string representing the image.
+ * @returns The image File object.
+ */
+function base64ToImageFile(base64: string,): File {
+    const bstr = atob(base64);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], 'avatar', { type: 'image/png' });
 }
 
 
 /**
- * Converts a base64 encoded image string to a File object.
- * @param imageString The base64 encoded image string.
- * @returns A Promise that resolves to a File object representing the image file.
+ * Converts a PNG file to a JPEG Blob.
+ * 
+ * @param pngFile - The PNG file to convert.
+ * @returns A Promise that resolves to the converted JPEG Blob.
  */
-async function convertBase64toImageFile(imageString: string) {
-    const base64String = "data:image/png;base64," + imageString
-    const data = await fetch(base64String);
-    const blob = await data.blob();
-    const file = await new File([blob], 'avatar', { type: 'image/png' });
-    return file
+function convertPngFileToJpegBlob(pngFile: File): Promise<Blob> {
+    return new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const url = URL.createObjectURL(pngFile);
+
+        img.onload = function () {
+            if (ctx) {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+                canvas.toBlob(blob => {
+                    URL.revokeObjectURL(url);
+                    if (blob) {
+                        resolve(blob);
+                    } else {
+                        reject(new Error('Failed to convert PNG to JPEG'));
+                    }
+                }, 'image/jpeg', 0.8);
+            }
+        };
+
+        img.onerror = function () {
+            URL.revokeObjectURL(url);
+            reject(new Error('Failed to load image'));
+        };
+
+        img.src = url;
+    });
 }
