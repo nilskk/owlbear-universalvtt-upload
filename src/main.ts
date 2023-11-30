3a149bc3e391b310b7dd43a3e71865d6d510c2ba
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2023-11-30 21:53:09 +0100

    separate html from code

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,26 @@
+// Import the CSS styles
 import './style.css'
-// import { setupScene } from './upload.ts'
+
+// Import the necessary functions from the 'importUniversalVTT.ts' module
 import { createSceneFromInput, uploadScene } from './importUniversalVTT.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h2>Upload a Dungeondraft Universal VTT File</h2>
-    <input type="file" id="avatar" accept=".dd2vtt" /><br><br>
-    <input type="checkbox" id="lightCheckbox">
-    <label for="lightCheckbox">Include Lightsources?</label><br><br>
-    <div id="simplification">
-      <input type="checkbox" id="simplificationCheckbox" checked>
-      <label for="simplificationCheckbox">Simplify walls?</label><br>
-      <label for="simplificationSlider">Simplification Ratio</label><br>
-      <input type="range" id="simplificationSlider" min="0.05" max="0.25" step="0.05" list="values" />
-      <datalist id="values">
-        <option value="0.05" label="0.05"></option>
-        <option value="0.1" label="0.1"></option>
-        <option value="0.15" label="0.15"></option>
-        <option value="0.2" label="0.2"></option>
-        <option value="0.25" label="0.25"></option>
-      </datalist><br>
-    </div>
-    
-    <button type="button" id="uploadButton">Create Scene</button>
-  
-  </div>
-  
-`
 
-// setupScene(document.querySelector<HTMLButtonElement>('#scene')!)
+// Call the 'createSceneFromInput' function with the file input element
+// This function will create a scene from the selected file
 createSceneFromInput(document.querySelector<HTMLInputElement>('#avatar')!)
+
+// Call the 'uploadScene' function with the necessary elements
+// This function will upload the created scene based on the selected options
 uploadScene(
-  document.querySelector<HTMLButtonElement>('#uploadButton')!, 
+  // The button that triggers the upload
+  document.querySelector<HTMLButtonElement>('#uploadButton')!,
+  
+  // The checkbox that determines whether to include light sources
   document.querySelector<HTMLInputElement>('#lightCheckbox')!,
+  
+  // The checkbox that determines whether to simplify walls
   document.querySelector<HTMLInputElement>('#simplificationCheckbox')!,
+  
+  // The slider that sets the simplification ratio
   document.querySelector<HTMLInputElement>('#simplificationSlider')!
 )
